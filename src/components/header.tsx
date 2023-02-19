a5635a8138f72c4b130bfe75ceaa42e9c1a34078
SejuneOh <etry0715@gmail.com>
2023-02-19 23:49:00 +0900

    feat: Add Header swr menu and make swr page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,6 +11,10 @@ const menues: Menu[] = [
     name: "i18next",
     path: "/i18next",
   },
+  {
+    name: "SWR",
+    path: "/swr",
+  },
 ];
 
 ///menu layout
@@ -19,9 +23,15 @@ export default function Header() {
     <>
       <div className=" w-full  min-h-[60px] bg-black text-white flex items-center">
         <ul className="flex justify-start items-center gap-3 px-3 ">
-          { menues? ( menues.map((menu, idx)=> {
-            return <Link className="hover:opacity-60" key={idx} href={menu.path}>{menu.name}</Link>
-          })): null}
+          {menues
+            ? menues.map((menu, idx) => {
+                return (
+                  <Link className="hover:opacity-60" key={idx} href={menu.path}>
+                    {menu.name}
+                  </Link>
+                );
+              })
+            : null}
         </ul>
       </div>
     </>
---
+++ src/pages/swr.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Swr() {
+  return <div>swr</div>;
+}
